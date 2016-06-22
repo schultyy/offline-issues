4f946495aded3cb691d980872ad1034958dd620d
Jan Schulte <hello@unexpected-co.de>
2016-06-22 23:40:32 +0200

    fix sw url

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@
 
   function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }).catch(function(err) {
