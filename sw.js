dcccceee92f7485852ce77d1957f05dd79ae73d7
Jan Schulte <hello@unexpected-co.de>
2016-06-22 23:35:34 +0200

    fix missing imports and eventhandlers

--- sw.js
+++ sw.js
@@ -2,6 +2,29 @@
 
 var CACHE_NAME = 'my-site-cache-v1';
 
+var urlsToCache = [
+  '/',
+  '/css/bootstrap.css',
+  '/css/app.css',
+  '/js/lodash.js',
+  '/js/jquery-2.2.4.js',
+  '/js/bootstrap.js',
+  '/js/github.bundle.min.js',
+  '/js/pouchdb-5.4.0.js',
+  '/js/pouchdb.all-dbs.js',
+  '/js/app.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
